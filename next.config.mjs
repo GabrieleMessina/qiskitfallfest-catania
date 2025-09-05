430317b4bfc0e4faee4e895e3f38f3a87094d957
Gabriele Messina <gabryelemessina99@gmail.com>
2025-09-05 10:33:55 +0000

    Fix next.js web site deploy settings

--- next.config.mjs
+++ next.config.mjs
@@ -4,12 +4,12 @@ const nextConfig = {
     output: 'export',
    
     // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-    // trailingSlash: true,
+    trailingSlash: true,
    
     // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
     // skipTrailingSlashRedirect: true,
    
     // Optional: Change the output directory `out` -> `dist`
-    distDir: 'dist',
+    distDir: 'cataniaqiskitfallfest',
   }
 export default nextConfig;
