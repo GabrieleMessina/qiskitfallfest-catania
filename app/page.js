b87780dd319d0f6ae7cb41ea99607da85f3f7748
Gabriele Messina <gabryelemessina99@gmail.com>
2025-10-28 11:01:54 +0000

    Add hackathon registration form

--- app/page.js
+++ app/page.js
@@ -21,7 +21,14 @@ export default function Home() {
         <div className="relative top-1/4 md:top-1/3 flex flex-col justify-center items-center font-bold h-[60%] w-[80%] md:h-[50%] md:w-[60%] bg-white/60 backdrop-blur-lg border border-white/20 shadow-lg rounded-lg p-6">
           <h1 className="text-3xl md:text-3xl lg:text-5xl 2xl:text-6xl text-[#353A3F] text-center">Second Edition</h1>
           <span className="md:text-lg lg:text-3xl 2xl:text-md text-[#9D5CFD] md:m-3 text-center">Catania joins the 55 Sponsored events worldwide!</span>
-          <button className="bg-[#9D5CFD] hover:bg-[#924afc] text-white rounded text-center text-lg md:text-2xl font-bold mt-10 flex justify-center md:w-[60%] p-5"><Link target='blank' href={'https://cataniaqiskitfallfest25.eventbrite.com'}>Join the Fest, get the ticket!</Link></button>
+          <button className="bg-[#9D5CFD] hover:bg-[#924afc] text-white rounded text-center text-lg md:text-2xl font-bold mt-5 flex justify-center md:w-[60%] p-5">
+            <Link target='blank' href={'https://cataniaqiskitfallfest25.eventbrite.com'}>Join the Fest, get the ticket!</Link>
+          </button>
+          <div id='register button' className='border border-[#9D5CFD] hover:bg-[#924afc11] rounded text-center text-lg md:text-2xl font-bold mt-5 flex justify-center md:w-[60%] p-5'>
+            <button> 
+              <Link target='blank' href="https://forms.gle/FxfRn3ThSmXtwo1c9">Register for the Hackathon!</Link>
+            </button>
+          </div>
         </div>
       </section>
       
